69544a5d4359319bb04b89f6e9fac2ff33a995a6
Oleksandr Stovban <stovbanolexandr@gmail.com>
2025-05-04 19:58:14 +0300

    Додав Get та Put

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -20,4 +20,53 @@ exports.createTask = async (req, res) => {
   }
 };
 
+// PUT /api/tasks/:id
+exports.updateTask = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    //console.log("ID:", id);
+    //console.log("BODY:", req.body);
 
+    const [updated] = await Task.update(req.body, { where: { id } });
+
+    if (updated) {
+      const updatedTask = await Task.findByPk(id);
+      res.json(updatedTask);
+    } else {
+      res.status(404).json({ error: "Task not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: "Failed to update task" });
+  }
+};
+
+// DELETE /api/tasks/:id
+exports.deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Task.destroy({ where: { id } });
+    if (deleted) {
+      res.json({ message: "Task deleted" });
+    } else {
+      res.status(404).json({ error: "Task not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete task" });
+  }
+};
+
+// GET /api/tasks/:id
+exports.getTaskById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByPk(id);
+    if (task) {
+      res.json(task);
+    } else {
+      res.status(404).json({ error: "Task not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch task" });
+  }
+};
